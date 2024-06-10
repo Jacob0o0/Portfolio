c579d23e5edbf8a55cf0fca25238bc38c0600080
Jacobo Escorcia <jacoboe127@gmail.com>
2024-06-09 22:51:05 -0600

    Components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import './styles/App.css';
+
+import { BrowserRouter, Router, Route } from 'react-router-dom';
+
+import Home from './components/Home'
+import Experience from './components/Experience'
+import Portfolio from './components/Portfolio'
+import Skills from './components/Skills'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// =============== STYLE ===============
+import '../styles/Home.css'
+
+// =============== ASSETS ===============
+import my_picture from '../assets/images/Me_happy.png'
+
+const Home = () => {
+  return (
+    <section className='home_page'>
+        <aside className='left'>
+            <div className="main_card">
+                {/* <img src={my_picture} alt="Jacobo Escorcia" className='main_picture'/> */}
+            </div>
+        </aside>
+        <aside className='right'>
+            <h3>Hello World, I'm</h3>
+            <h1>Jacobo Escorcia,</h1>
+            <h2 className='gradient-text'>Application Developer</h2>
+        </aside>
+    </section>
+  )
+}
+
+export default Home
