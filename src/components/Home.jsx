2507e565c6363e502a6db121d1941ff53463590a
Jacobo Escorcia <jacoboe127@gmail.com>
2024-06-10 21:14:17 -0600

    Components created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,17 +8,17 @@ import my_picture from '../assets/images/Me_happy.png'
 
 const Home = () => {
   return (
-    <section className='home_page'>
-        <aside className='left'>
-            <div className="main_card">
+    <section className='home_page debbug'>
+        {/* <aside className='left'>
+            <div className="main_card"> */}
                 {/* <img src={my_picture} alt="Jacobo Escorcia" className='main_picture'/> */}
-            </div>
+            {/* </div>
         </aside>
         <aside className='right'>
             <h3>Hello World, I'm</h3>
             <h1>Jacobo Escorcia,</h1>
             <h2 className='gradient-text'>Application Developer</h2>
-        </aside>
+        </aside> */}
     </section>
   )
 }
